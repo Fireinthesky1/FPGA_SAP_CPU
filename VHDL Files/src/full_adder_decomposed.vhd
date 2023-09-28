39a9b8e2f9b3b1c4ea564229289ce4d943b480d2
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-09-27 19:22:35 -0700

    adder stuff

---
+++ VHDL Files/src/full_adder_decomposed.vhd
@@ -0,0 +1,4 @@
+-- VHDL 8 bit processor Project
+-- Full Adder (decomposed)
+-- James Hicks Sept 21 2023
+
---
+++ VHDL Files/src/half_adder_tb.vhd
@@ -0,0 +1,50 @@
+-- VHDL 8 bit processor Project
+-- Half Adder Test Bench
+-- James Hicks Sept 22 2023
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- a testbench has no ports
+entity half_adder_tb is
+end half_adder_tb;
+
+architecture tb_architecture of half_adder_tb is
+    component half_adder
+        port (
+                i1, i0 : in std_logic;
+                sum, carry : out std_logic
+             );
+    end component;
+
+    signal test_bits_in : std_logic_vector(1 downto 0);
+    signal test_bits_out : std_logic_vector (1 downto 0);
+begin
+    DUT : half_adder port map (
+                                        i1 => test_bits_in(1),
+                                        i0 => test_bits_in(0),
+                                        carry => test_bits_out(1),
+                                        sum => test_bits_out(0)
+                                     );
+    process begin
+        -- test vector 1 --
+        test_bits_in <= "00";
+        wait for 1 ns;
+
+        -- test vector 2 --
+        test_bits_in <= "01";
+        wait for 1 ns;
+
+        -- test vector 3 --
+        test_bits_in <= "10";
+        wait for 1 ns;
+
+        -- test vector 4 --
+        test_bits_in <= "11";
+        wait for 1 ns;
+
+        assert false report "End of Test";
+        wait;
+
+    end process;
+end tb_architecture;
