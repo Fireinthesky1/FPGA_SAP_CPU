6ee3695b9fd43024bbc58ef27d68189753fe1bbe
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-01 19:16:56 -0700

    Eight Bit Bitwise And Tested and Debugged

--- VHDL Files/src/Bitwise Logic/eight_bit_bitwise_and_tb.vhd
+++ VHDL Files/src/Bitwise Logic/eight_bit_bitwise_and_tb.vhd
@@ -14,7 +14,7 @@ architecture tb_architecture of eight_bit_bitwise_and_tb is
     -- 16 bits in, 8 bits out
     signal test_bits_in_a : std_logic_vector (7 downto 0);
     signal test_bits_in_b : std_logic_vector (7 downto 0);
-    signal test_bits_out : std_logic_vector (7 donto 0);
+    signal test_bits_out  : std_logic_vector (7 downto 0);
 
 begin
     DUT : entity work.eight_bit_bitwise_and(my_arch)
@@ -26,7 +26,7 @@ begin
     process begin
         for I in 0 to 255 loop
             test_bits_in_a <= std_logic_vector(to_unsigned(I, 8));
-            test_bits_in_b <= std_logic_vector(to_unsigned(255 - I, 8));
+            test_bits_in_b <= std_logic_vector(to_unsigned(I, 8));
             wait for 1 ns;
         end loop;
 
