0b12c05849722be17cc73df25eb0a0d031f8f2cf
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-08 09:46:33 -0700

    messed around

--- VHDL Files/src/8 Bit Adder/4 bit Adder/four_bit_adder_subtractor_tb.vhd
+++ VHDL Files/src/8 Bit Adder/4 bit Adder/four_bit_adder_subtractor_tb.vhd
@@ -16,23 +16,62 @@ end four_bit_adder_subtractor_tb;
 architecture tb_architecture of four_bit_adder_subtractor_tb is
 
 -- 9 inputs (2 4-bit numbers + control) 5 outputs (a 4 bit number + carry)
-    signal test_bits_in : std_logic_vector (8 downto 0);
-    signal test_bits_out : std_logic_vector (4 downto 0);
+    signal control      : std_logic;
+    signal a,b          : std_logic_vector (3 downto 0);
+    signal carry_out    : std_logic;
+    signal sum_diff     : std_logic_vector (3 downto 0);
 
 begin
     DUT : entity work.four_bit_adder_subtractor(my_arch)
         port map (
-                    control => test_bits_in (8),
-                    a => test_bits_in (7 downto 4),
-                    b => test_bits_in (3 downto 0),
-                    carry_out => test_bits_out (4),
-                    sum_diff => test_bits_out (3 downto 0)
+                    control => control,
+                    a => a,
+                    b => b,
+                    carry_out => carry_out,
+                    sum_diff => sum_diff
                  );
     process begin
-        for I in 255 to 512 loop
-            test_bits_in <= std_logic_vector(to_unsigned(I,9));
-            wait for 1 ns;
-        end loop;
+        -- test 4 + 4 = 8 (out of range for signed 4 bit number)
+        control <= '0';
+        a <= "0100";
+        b <= "0100";
+        wait for 1 ns;
+
+        -- test for -4 + -5 = -9 (out of range for signed 4 bit number)
+        control <= '0';
+        a <= "1100";
+        b <= "1011";
+        wait for 1 ns;
+
+        -- test adding positive and negative extremes 7 + -8 = -1
+        control <= '0';
+        a <= "0111";
+        b <= "1000";
+        wait for 1 ns;
+
+        -- test for subtraction overflow 3 - -6 = 9 (out of range)
+        control <= '1';
+        a <= "0101";
+        b <= "1010";
+        wait for 1 ns;
+
+        -- test for subtraction underflow -7 - 7 (out of range)
+        control <= '1';
+        a <= "1111";
+        b <= "0111";
+        wait for 1 ns;
+
+        -- test for 0 - 1 = -1
+        control <= '1';
+        a <= "0000";
+        b <= "0001";
+        wait for 1 ns;
+
+        -- test for 1 - -1 = 0
+        control <= '1';
+        a <= "1111";
+        b <= "1111";
+        wait for 1 ns;
 
         assert false report "End of Test";
         wait;
